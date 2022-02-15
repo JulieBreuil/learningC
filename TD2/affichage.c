ffa053553e24539cbacc441c18ffc6649328ac56
JulieBreuil <jbreuil@tc405-109-08.insa-lyon.fr>
2022-02-15 16:43:09 +0100

    TD2q21_ajoutAlignementDroite

--- TD2/affichage.c
+++ TD2/affichage.c
@@ -3,6 +3,7 @@
 int main(){
     for (int i=0; i<30; i++){
         int c = i*i;
-        printf("Le carré de %d est %d\n",i,c);
+        printf("Le carré de %4d est %4d\n",i,c);
+        //le %4d permet l'alignement des chiffres sur la droite
     }
 }
